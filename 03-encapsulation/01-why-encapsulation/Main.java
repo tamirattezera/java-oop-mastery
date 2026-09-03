0831a9291019fe2afd98647da9263b429f230f17
Tamirat Tezera <tamirattezera8@gmail.com>
2026-09-03 17:14:43 +0300

    feat: add encapsulation why experiment

--- 03-encapsulation/01-why-encapsulation/Main.java
+++ 03-encapsulation/01-why-encapsulation/Main.java
@@ -1,18 +1,32 @@
 class BankAccount {
-    public double balance;
 
+    public double balance;
 }
+
 public class Main {
 
     public static void main(String[] args) {
+
         BankAccount account = new BankAccount();
 
         account.balance = 1000;
 
-        System.out.println("Initial balance:" + account.balance);
+        System.out.println("Initial balance: " + account.balance);
 
-        account.balance = -5000;
+        withdraw(account);
 
-        System.out.println("Updated balance:" + account.balance);
+        System.out.println("After withdrawal: " + account.balance);
+
+        randomSystemUpdate(account);
+
+        System.out.println("Final balance: " + account.balance);
+    }
+
+    public static void withdraw(BankAccount account) {
+        account.balance = account.balance - 200;
+    }
+
+    public static void randomSystemUpdate(BankAccount account) {
+        account.balance = -999999;
     }
 }
