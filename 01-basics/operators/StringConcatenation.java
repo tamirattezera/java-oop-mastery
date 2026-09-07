c548ac3740442c7e98eb387f8dc214111500116a
Tamirat Tezera <tamirattezera8@gmail.com>
2026-09-07 12:47:53 +0300

    feat: add string concatenation experiment

---
+++ 01-basics/operators/StringConcatenation.java
@@ -0,0 +1,12 @@
+package operators;
+
+public class StringConcatenation {
+
+    public  static  void main(String[] args) {
+
+        System.out.println(10 + 20);
+        System.out.println("Age: " + 22);
+        System.out.println("Hello " + "Java");
+    }
+    
+}
