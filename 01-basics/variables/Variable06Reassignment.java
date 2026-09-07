00d3776b0ba17b5ed1f397e916f07d53ce13e66d
Tamirat Tezera <tamirattezera8@gmail.com>
2026-09-07 10:19:06 +0300

    feat: demonstrate variable reassignment

---
+++ 01-basics/variables/Variable06Reassignment.java
@@ -0,0 +1,16 @@
+package variables;
+
+public class Variable06Reassignment {
+
+public static void main(String[] args) {
+
+    int age = 22;
+
+    System.out.println(age);
+
+    age = 23;
+
+    System.out.println(age);
+}
+
+}
