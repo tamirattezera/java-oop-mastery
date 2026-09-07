eb1a3003edeafc5f6787642c62c1a315c537ca18
Tamirat Tezera <tamirattezera8@gmail.com>
2026-09-07 12:23:52 +0300

    feat: add integer and floating-point division experiment

---
+++ 01-basics/operators/DivisionBehavior.java
@@ -0,0 +1,13 @@
+package operators;
+
+public class DivisionBehavior {
+
+    public  static  void main(String[] args) {
+
+        System.out.println(7 / 2);
+        System.out.println(7.0 / 2);
+        System.out.println(7 / 2.0);
+        System.out.println(7.0 / 2.0);
+    }
+    
+}
