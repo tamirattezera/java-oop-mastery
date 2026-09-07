94e7b823cf8cdd11993e052ad94de8b0abdf8199
Tamirat Tezera <tamirattezera8@gmail.com>
2026-09-07 12:30:12 +0300

    feat: add remainder operator experiment

---
+++ 01-basics/operators/RemainderOperator.java
@@ -0,0 +1,12 @@
+package operators;
+
+public class RemainderOperator {
+    public  static  void  main(String[] args) {
+        
+        System.out.println(10 % 2);
+        System.out.println(11 % 2);
+        System.out.println(15 % 4);
+        System.out.println(20 % 6);
+    }
+    
+}
