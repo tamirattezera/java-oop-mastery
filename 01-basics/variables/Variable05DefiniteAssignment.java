d059e6212017122bd487988c6c2f66b939f3f621
Tamirat Tezera <tamirattezera8@gmail.com>
2026-09-07 10:17:28 +0300

    test: explore definite assignment analysis

---
+++ 01-basics/variables/Variable05DefiniteAssignment.java
@@ -0,0 +1,18 @@
+package variables;
+
+public class Variable05DefiniteAssignment {
+
+public static void main(String[] args) {
+
+    int age;
+
+    boolean shouldAssign = true;
+
+    if (shouldAssign) {
+        age = 22;
+    }
+
+    System.out.println(age);
+}
+
+}
