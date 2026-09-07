0b2a1d2a17443cbf05032a0101dd38a5d5a3a762
Tamirat Tezera <tamirattezera8@gmail.com>
2026-09-07 10:20:18 +0300

    feat: add multiple variable experiment

---
+++ 01-basics/variables/Variable07MultipleVariables.java
@@ -0,0 +1,15 @@
+package variables;
+
+public class Variable07MultipleVariables {
+
+public static void main(String[] args) {
+
+    int age = 22;
+    int score = 95;
+
+    System.out.println(age);
+    System.out.println(score);
+}
+
+
+}
