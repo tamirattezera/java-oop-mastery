43b34227b246a1e13493646f0c88450dac1afe12
Tamirat Tezera <tamirattezera8@gmail.com>
2026-09-07 12:06:43 +0300

    feat: add arithmetic addition experiment

---
+++ 01-basics/operators/Operator01Addition.java
@@ -0,0 +1,15 @@
+package operators;
+
+public class Operator01Addition {
+
+    public  static void main(String[] args) {
+
+        int price = 100;
+        int quantity = 3;
+
+        int total = price + quantity;
+
+        System.out.println(total);
+    }
+    
+}
