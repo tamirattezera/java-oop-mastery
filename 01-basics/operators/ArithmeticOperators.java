3a3edcf475fa4ce1d677f06490c19fac6f615141
Tamirat Tezera <tamirattezera8@gmail.com>
2026-09-07 12:16:28 +0300

    feat: add arithmetic operators experiment

---
+++ 01-basics/operators/ArithmeticOperators.java
@@ -0,0 +1,19 @@
+package operators;
+public class ArithmeticOperators {
+
+    public static void main(String[] args) {
+
+        int a = 20;
+        int b = 4;
+
+        int sum = a + b;
+        int difference = a - b;
+        int product = a * b;
+        int quotient = a / b;
+
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + difference);
+        System.out.println("Product: " + product);
+        System.out.println("Quotient: " + quotient);
+    }
+}
