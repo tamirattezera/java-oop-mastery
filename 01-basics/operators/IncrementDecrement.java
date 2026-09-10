7cd183ef9e1a579ebc16a6b3f3714469d63249ac
Tamirat Tezera <tamirattezera8@gmail.com>
2026-09-10 07:01:11 +0300

    feat: add increment and decrement experiment

---
+++ 01-basics/operators/IncrementDecrement.java
@@ -0,0 +1,15 @@
+package operators;
+
+public class IncrementDecrement {
+    public static  void main(String[] args) {
+
+        int count = 5;
+
+        count++;
+        System.out.println(count);
+
+        count--;
+        System.out.println(count);
+    }
+    
+}
